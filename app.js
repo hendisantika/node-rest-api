9ecafb02740b99f9187f8e8b7c9c16e98880f8eb
Hendi Santika <hendisantika@gmail.com>
2018-11-09 06:01:13 +0700

    #3 - Add Mongoose requirement

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/product')
+    .then(() => console.log('connection successfull!'))
+    .catch((err) => console.error(err));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
